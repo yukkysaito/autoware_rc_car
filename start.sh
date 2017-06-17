b2c45c88c7c0e4a73bf8187be1a0dfa959e4784a
Yukihiro SAITO <yukky.saito@gmail.com>
2017-06-17 23:38:31 +0900

    Change start script

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
-cd ~/workspace/Autoware/ros/
+cd ~/Autoware/ros/
 ./run
 
 sleep 3
 
-roslaunch razor_imu_9dof razor-pub.launch&
-rosrun rosserial_python serial_node.py _port:=/dev/ArduinoNano&
-roslaunch realsense_camera r200_nodelet_rgbd.launch  &
-source ~/workspace/catkin_ws/devel/setup.sh
-roslaunch twist_cmd_converter twist_cmd_converter.launch &
-roslaunch tf_publisher tf_local.launch &
+source ~/workspace/autoware_rc_car/catkin_ws/devel/setup.sh
+roslaunch demo_launch demo.launch 
